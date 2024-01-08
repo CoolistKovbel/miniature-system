284660fedefc706088db2344519168d80e7eb4a1
coolistkovbel <coolistkovbel@gmail.com>
2024-01-08 11:27:08 -0500

    env expose... depression

--- actions/register.ts
+++ actions/register.ts
@@ -3,6 +3,8 @@ import { RegisterSchema } from "@/schemas";
 // Just like a api route but from nextauth 14
 
 import * as z from "zod";
+import { hash } from "bcryptjs";
+import { db } from "@/lib/db";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -10,6 +12,29 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
   if (!validatedFields.success) {
     return { error: "invalud field" };
   }
+
+
+  const {email, password, username} = validatedFields.data
+
+  const hashPassword = await hash(password, 10)
+
+
+  const existingUser = await db.user.findUnique({where: {email}})
+
+
+  if(existingUser) {
+    return {error: "Sorry this exists"}
+  }
+
+  await db.user.create({
+    data: {
+      email,
+      password: hashPassword,
+      username
+    }
+  })
+
+  // Send email
 
   return { success: "email sent!" };
 };
